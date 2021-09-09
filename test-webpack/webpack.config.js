5d385353391480f9df048736392c4138dd93de31
youzouzou <guifang.you@tcl.com>
2021-09-09 16:09:16 +0800

    发布插件

--- test-webpack/webpack.config.js
+++ test-webpack/webpack.config.js
@@ -1,6 +1,7 @@
 // test-webpack/webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const YzzWebpackTestPlugin = require("yzz-webpack-test-plugin"); // 引入线上版本插件
 const YzzWebpackTestPlugin = require(path.resolve("../yzz-webpack-test-plugin")); // 这里用的是本地的相对路径引入
 
 module.exports = {
@@ -16,9 +17,9 @@ module.exports = {
     }),
     // 邮件发送参数参见https://nodemailer.com/about/
     new YzzWebpackTestPlugin({
-      fromEmail: "xxx@qq.com", // 发送方邮箱
-      password: "xxxx", // 如果是QQ邮箱，则为QQ邮箱授权码
-      toEmail: "xxx@xx.com", // 接收方邮箱
+      fromEmail: "xx@qq.com", // 发送方邮箱
+      password: "xxx", // 如果是QQ邮箱，则为QQ邮箱授权码
+      toEmail: "xx@163.com", // 接收方邮箱
       host: "smtp.qq.com"
     })
   ],
